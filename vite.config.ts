8e4d1d0c2bd295a5367e2d75356fdf10b1df8be8
장서은 <sejang@ymx.co.kr>
2026-02-23 17:02:04 +0900

    shadcn 설정중..(tsconfig.app.json까지 설정)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
-})
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
